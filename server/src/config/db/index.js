e1ce414c16a8f72553907b994c65096494706415
ClementeSerrano <clementeserranosutil@gmail.com>
2019-02-26 23:06:32 -0300

    fix(meAsCompany non user res): Fixed meAsCompany query res when no user is in session (to prevent breaking the client when logging out)

--- server/src/config/db/index.js
+++ server/src/config/db/index.js
@@ -4,9 +4,6 @@ require("dotenv").config({ path: ".env" });
 mongoose.set("useCreateIndex", true);
 
 mongoose
-  .connect(
-    process.env.DB_URL,
-    { useNewUrlParser: true }
-  )
+  .connect(process.env.DB_URL, { useNewUrlParser: true })
   .then(() => console.log("🍌 DB running..."))
   .catch(error => console.log(error));
--- server/src/imports/apollo/resolvers/Query/Company/meAsCompany/index.js
+++ server/src/imports/apollo/resolvers/Query/Company/meAsCompany/index.js
@@ -3,10 +3,9 @@ import helpers from "../../../../../helpers";
 const { requestAuth } = helpers;
 
 export default async (_, args, { user }) => {
-  try {
-    const me = await requestAuth(user);
-    return me;
-  } catch (error) {
-    throw error;
+  if (!user) {
+    return null;
   }
+  const me = await requestAuth(user);
+  return me;
 };
