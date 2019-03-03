a35397fc98df225adbc9400e332d6ab7e3a548b5
Clemente Serrano <clementeserranosutil@gmail.com>
2019-03-03 18:41:14 -0300

    feature(wallet): Wallet page now connected with servere for balance queries

--- payments-client/src/app/pages/auth/Wallet/index.js
+++ payments-client/src/app/pages/auth/Wallet/index.js
@@ -1,6 +1,49 @@
 import React from "react";
+import { Query } from "react-apollo";
 
-export default ({ myData }) => {
-  console.log(myData);
-  return <div>Wallet section</div>;
+import { QUERIES } from "../../../apollo";
+import { Card } from "../../../components";
+import {
+  CryptoFundsContainer,
+  BalanceContainer,
+  MainContainer,
+  MarketContainer
+} from "./styles";
+
+const { GET_WALLET } = QUERIES;
+
+const Wallet = ({ userData, walletData }) => {
+  return (
+    <MainContainer>
+      <CryptoFundsContainer>
+        <Card
+          title="Crypto funds"
+          content={<p>Pending cryptos</p>}
+          buttons={null}
+        />
+      </CryptoFundsContainer>
+      <BalanceContainer>
+        <Card
+          title="Balance"
+          content={<p>{walletData.getWallet.balance} CLP</p>}
+          buttons={null}
+        />
+      </BalanceContainer>
+      <MarketContainer>
+        <Card title="Markets" content={<p>Pending markets</p>} buttons={null} />
+      </MarketContainer>
+    </MainContainer>
+  );
 };
+
+export default ({ myData }) => (
+  <Query query={GET_WALLET} variables={{ ownerId: myData._id }}>
+    {({ data, loading, error }) => {
+      if (loading) return <p>Loading...</p>;
+
+      if (error) return <p>Error!</p>;
+
+      return <Wallet userData={myData} walletData={data} />;
+    }}
+  </Query>
+);
---
+++ payments-client/src/app/pages/auth/Wallet/styles.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+import { colors } from "../../../utils";
+
+const { white } = colors;
+
+const MainContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 60px 50px;
+`;
+
+const CryptoFundsContainer = styled.div``;
+
+const BalanceContainer = styled.div``;
+
+const MarketContainer = styled.div``;
+
+export {
+  CryptoFundsContainer,
+  BalanceContainer,
+  MainContainer,
+  MarketContainer
+};
