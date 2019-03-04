e265dd68b298e7a07da81571492c4eb23fa9048f
Clemente Serrano <clementeserranosutil@gmail.com>
2019-03-03 22:53:48 -0300

    chore(env vars): Added orionx keys env vars

--- server/src/config/cryptoxchange/index.js
+++ server/src/config/cryptoxchange/index.js
@@ -1,7 +1,8 @@
 import Orionx from "orionx-sdk";
+require("dotenv").config({ path: ".env" });
 
 Orionx.setCredentials({
-  apiKey: "NyCQb5cYvJP4DwtWN6uaxgBmtNyrQhyEZB",
-  secretKey: "obwFw6TCsjXY5PBy34xHXr6cLjqE7qS3WY",
-  apiUri: "https://api2.orionx.io/graphql"
+  apiKey: process.env.ORIONX_APIKEY,
+  secretKey: process.env.ORIONX_SECRET_KEY,
+  apiUri: process.env.ORIONX_API_URL
 });
