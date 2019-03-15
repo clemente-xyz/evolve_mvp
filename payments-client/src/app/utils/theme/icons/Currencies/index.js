7325c1e0cb08d51ae7fbb24020388ffea3a3b9ec
ClementeSerrano <clementeserranosutil@gmail.com>
2019-03-15 08:30:22 -0300

    feature(altcoins icons): Added basic altcoins svg icons

--- payments-client/src/app/utils/theme/icons/Currencies/index.js
+++ payments-client/src/app/utils/theme/icons/Currencies/index.js
@@ -1,5 +1,19 @@
 import Bitcoin from "./Bitcoin";
+import BitcoinCash from "./BitcoinCash";
+import Dash from "./Dash";
+import Ether from "./Ether";
+import Litecoin from "./Litecoin";
+import Ripple from "./Ripple";
+import Stellar from "./Stellar";
+import Tron from "./Tron";
 
 export default {
-  Bitcoin
+  Bitcoin,
+  BitcoinCash,
+  Dash,
+  Ether,
+  Litecoin,
+  Ripple,
+  Stellar,
+  Tron
 };
