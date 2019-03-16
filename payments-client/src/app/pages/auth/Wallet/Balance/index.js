b6e699cbe83f9b18dad03605b475dd9f214a68df
ClementeSerrano <clementeserranosutil@gmail.com>
2019-03-16 18:15:08 -0300

    style(balance): Add grid system to content container

--- payments-client/src/app/pages/auth/Wallet/Balance/index.js
+++ payments-client/src/app/pages/auth/Wallet/Balance/index.js
@@ -1,40 +1,56 @@
 import React from "react";
 
 import { Button, Card } from "../../../../components";
-import { colors } from "../../../../utils";
+import { colors, helpers } from "../../../../utils";
+import {
+  AmountContainer,
+  BalanceAmount,
+  ButtonsContainer,
+  ContentContainer
+} from "./styles";
 
+const { formatAmount } = helpers;
 const { white, green, dark_green, blue, dark_blue } = colors;
 
 export default ({ balanceAmount }) => {
+  const formatedBalanceAmount = formatAmount(balanceAmount);
+
   return (
     <Card
       title={{
         text: "Balance",
         alignment: "left"
       }}
-      content={<p>{balanceAmount} CLP</p>}
-      buttons={
-        <>
-          <Button
-            text="Deposit"
-            onClick={() => {
-              alert("Deposit");
-            }}
-            textColor={white}
-            backgroundColor={blue}
-            hoverColor={dark_blue}
-          />
-          <Button
-            text="Withdraw"
-            onClick={() => {
-              alert("Withdraw");
-            }}
-            textColor={white}
-            backgroundColor={green}
-            hoverColor={dark_green}
-          />
-        </>
+      content={
+        <ContentContainer>
+          <AmountContainer>
+            <BalanceAmount>{formatedBalanceAmount}</BalanceAmount>
+            CLP
+          </AmountContainer>
+
+          <ButtonsContainer>
+            <Button
+              text="Deposit"
+              onClick={() => {
+                alert("Deposit");
+              }}
+              textColor={white}
+              backgroundColor={blue}
+              hoverColor={dark_blue}
+            />
+            <Button
+              text="Withdraw"
+              onClick={() => {
+                alert("Withdraw");
+              }}
+              textColor={white}
+              backgroundColor={green}
+              hoverColor={dark_green}
+            />
+          </ButtonsContainer>
+        </ContentContainer>
       }
+      buttons={null}
     />
   );
 };
---
+++ payments-client/src/app/pages/auth/Wallet/Balance/styles.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+const ContentContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const AmountContainer = styled.div`
+  flex: 0.6;
+`;
+
+const ButtonsContainer = styled.div`
+  display: flex;
+  flex: 0.7;
+  justify-content: space-between;
+`;
+
+const BalanceAmount = styled.span`
+  font-weight: 900;
+  margin-right: 10px;
+`;
+
+export { AmountContainer, BalanceAmount, ButtonsContainer, ContentContainer };
